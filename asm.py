279e8bc5a2937dbe120f5f308d75dd0f8eee86bc
jakob753951 <jlm1999@live.dk>
2021-09-06 19:11:51 +0200

    Reworked to JUMP command to use marks, to allow for easier program-writing

--- asm.py
+++ asm.py
@@ -1,97 +1,17 @@
 import argparse
 from os.path import exists
-import re
+from compiler import compile_vole
 
-parser = argparse.ArgumentParser(description='Transpile JASM to a VOLE program.')
+parser = argparse.ArgumentParser(description='Transpile ASMJ to a VOLE program.')
 parser.add_argument('source', type=str)
 parser.add_argument('destination', type=str)
+parser.add_argument('-s', '--starting_position', type=str, default='C0')
 args = parser.parse_args()
 
-class ARG_TYPE:
-	ADDRESS  = 0
-	LITERAL  = 1
-	REGISTER = 2
-
-operators = [
-	'IADD',
-	'FADD',
-	'OR',
-	'AND',
-	'XOR'
-]
-
-def op_2_code(operator: str) -> str:
-	return hex(operators.index(operator)+5)[2:]
-
-def addr_value(word: str) -> str:
-	match = re.search(r'(?<=\[)[0-9a-fA-F]{2}(?=\])', word)
-	if not match:
-		return None
-	return match[0]
-
-
-def get_arg_type(word):
-	if addr_value(word):
-		return ARG_TYPE.ADDRESS
-	if len(word) == 1:
-		return ARG_TYPE.REGISTER
-	return ARG_TYPE.LITERAL
-
-def move(words):
-	src_type, dst_type = [get_arg_type(word) for word in words[1:]]
-	if src_type == ARG_TYPE.ADDRESS and dst_type == ARG_TYPE.REGISTER:
-		return f'1{words[2]}{addr_value(words[1])}'
-	if src_type == ARG_TYPE.LITERAL and dst_type == ARG_TYPE.REGISTER:
-		return f'2{words[2]}{words[1]}'
-	if src_type == ARG_TYPE.REGISTER and dst_type == ARG_TYPE.ADDRESS:
-		return f'3{words[1]}{addr_value(words[2])}'
-	if src_type == ARG_TYPE.REGISTER and dst_type == ARG_TYPE.REGISTER:
-		return f'40{words[1]}{words[2]}'
-
-def operator(words: list[str]):
-	operator, a, b, dest = words
-	return f'{op_2_code(operator)}{dest}{a}{b}'
-
-def rotate(words):
-	return f'A{words[1]}0{words[2]}'
-
-def jump(words): # JUMP R [XY] = BRXY
-	dest = addr_value(words[2])
-	if not dest:
-		raise Exception('Address not read correctly')
-	return f'B{words[1]}{dest}'
-
-def halt_and_catch_fire(_):
-	pass
-
-instructions = {
-	'MOVE': move,
-	'IADD': operator,
-	'FADD': operator,
-	'OR': operator,
-	'AND': operator,
-	'XOR': operator,
-	'ROTATE': rotate,
-	'JUMP': jump,
-	'HALT': lambda _: 'C000',
-	'HCF': halt_and_catch_fire
-}
-def r_pad(s: str, length: int):
-	while len(s) < length:
-		s += '0'
-	return s
-
-def convert_line(l: str):
-	l = l.split('//')[0] #Ignore comments
-	l.replace(',', '')   #Remove commas
-	words = l.split(' ')
-	return instructions[words[0]](words)
-
 with open(args.source, 'r') as in_file:
-	src_lines = in_file.read().splitlines()
+	src_txt = in_file.read()
 
-out_strings = ['[PC]C0', '[C0]']
-out_strings += [convert_line(line) for line in src_lines]
+vole_code = compile_vole(src_txt, int(args.starting_position, 16))
 
 with open(args.destination, 'w' if exists(args.destination) else 'x') as out_file:
-	out_file.write('\n'.join(out_strings))
+	out_file.write(vole_code)
---
+++ compiler.py
@@ -0,0 +1,105 @@
+import re
+
+
+class ARG_TYPE:
+	ADDRESS  = 0
+	LITERAL  = 1
+	REGISTER = 2
+
+operators = [
+	'IADD',
+	'FADD',
+	'OR',
+	'AND',
+	'XOR'
+]
+
+marks = {}
+
+def set_marks(lines: list[str], start_pos: int):
+	out_lines = []
+	i = 0
+	for line in lines:
+		match = re.search(r'\S+(?=:\s?)', line)
+		if match:
+			marks[match[0]] = i + start_pos
+			continue
+		out_lines.append(line)
+		i += 2
+	
+	return out_lines
+
+def int_2_hex(num: int) -> str:
+	return hex(num)[2:].upper()
+
+def op_2_code(operator: str) -> str:
+	return hex(operators.index(operator)+5)[2:]
+
+def addr_value(word: str) -> str:
+	match = re.search(r'(?<=\[)[0-9a-fA-F]{2}(?=\])', word)
+	if not match:
+		return None
+	return match[0]
+
+
+def get_arg_type(word: str) -> ARG_TYPE:
+	if addr_value(word):
+		return ARG_TYPE.ADDRESS
+	if len(word) == 1:
+		return ARG_TYPE.REGISTER
+	return ARG_TYPE.LITERAL
+
+def move(words: list[str]) -> str:
+	src_type, dst_type = [get_arg_type(word) for word in words[1:]]
+	if src_type == ARG_TYPE.ADDRESS and dst_type == ARG_TYPE.REGISTER:
+		return f'1{words[2]}{addr_value(words[1])}'
+	if src_type == ARG_TYPE.LITERAL and dst_type == ARG_TYPE.REGISTER:
+		return f'2{words[2]}{words[1]}'
+	if src_type == ARG_TYPE.REGISTER and dst_type == ARG_TYPE.ADDRESS:
+		return f'3{words[1]}{addr_value(words[2])}'
+	if src_type == ARG_TYPE.REGISTER and dst_type == ARG_TYPE.REGISTER:
+		return f'40{words[1]}{words[2]}'
+	raise Exception('Address not read correctly')
+
+def operator(words: list[str]) -> str:
+	operator, a, b, dest = words
+	return f'{op_2_code(operator)}{dest}{a}{b}'
+
+def rotate(words: list[str]) -> str:
+	return f'A{words[1]}0{words[2]}'
+
+def jump(words: list[str]) -> str: # JUMP R mark = BRXY
+	dest_mark = words[2]
+	dest_str = int_2_hex(marks[dest_mark])
+	return f'B{words[1]}{dest_str}'
+
+def halt_and_catch_fire(_):
+	pass
+
+instructions = {
+	'MOVE': move,
+	'IADD': operator,
+	'FADD': operator,
+	'OR': operator,
+	'AND': operator,
+	'XOR': operator,
+	'ROTATE': rotate,
+	'JUMP': jump,
+	'HALT': lambda _: 'C000',
+	'HCF': halt_and_catch_fire,
+	'HACF': halt_and_catch_fire
+}
+
+def convert_line(l: str):
+	l = l.split('//')[0] #Ignore comments
+	l.replace(',', '')   #Remove commas
+	words = l.strip().split(' ')
+	return instructions[words[0]](words)
+
+def compile_vole(src: str, start_pos: int):
+	src_lines = src.splitlines()
+	src_lines = set_marks(src_lines, start_pos)
+	start_str = int_2_hex(start_pos)
+	out_lines = [f'[PC]{start_str}', f'[{start_str}]']
+	out_lines.extend([convert_line(line) for line in src_lines])
+	return '\n'.join(out_lines).upper()
